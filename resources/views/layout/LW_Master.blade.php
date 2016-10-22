92bb7dfee34ded1721a44f7d0ae30556ecc32e8d
Kevin <whiteowl0001@gmail.com>
2016-10-22 05:43:11 +0000

    lone wolf2

--- resources/views/layout/LW_Master.blade.php
+++ resources/views/layout/LW_Master.blade.php
@@ -3,8 +3,8 @@
 <head>
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
     <script src="https://use.fontawesome.com/01c0eac664.js"></script>
-    <link rel="stylesheet" type="text/css" href="css/bootstrap-social.css">
-    <link rel="stylesheet" type="text/css" href="css/custom.css">
+    <link rel="stylesheet" type="text/css" href="css/css_lonewolf/bootstrap-social.css">
+    <link rel="stylesheet" type="text/css" href="css/css_lonewolf/LW_custom.css">
 </head>
 
 <body>
