0f263c780d0c415d0c7f03daf1dc1a21f1a4fb6e
Kevin <whiteowl0001@gmail.com>
2016-10-26 06:55:54 +0000

    mail-5

--- resources/views/contact.blade.php
+++ resources/views/contact.blade.php
@@ -12,7 +12,7 @@
 
 
     <div class="container-fluid container-contact">
-    	<form action="{{ route('sendMail') }}" method="post">
+    	<form action="{{ url('sendMail') }}" method="post">
     		<input type="email" name="email" id="email"  placeholder="email address">
     		<input type="text"  name="title" id="title" placeholder="title">
     		<button type="submit"> Send Mail </button>
