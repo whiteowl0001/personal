2e1e72e69c4bc17ee656e573e526062a67dce5a2
Kevin <whiteowl0001@gmail.com>
2016-10-28 05:42:03 +0000

    Added Add Page

--- resources/views/auth/portal.blade.php
+++ resources/views/auth/portal.blade.php
@@ -4,7 +4,7 @@
     <div class="row">
         <div class="col-md-8 col-md-offset-2">
             <div class="panel panel-default">
-                <div class="panel-heading">Login</div>
+                <div class="panel-heading">Projects</div>
                 <div class="panel-body">
 		<div class="col-md-6">
                 <div class="hovereffect portfolioImage">
