f628392392fe985b95b0b2fc47900181ae303a97
Kevin <whiteowl0001@gmail.com>
2016-10-28 05:42:17 +0000

    Added Add Page

--- resources/views/auth/portal.blade.php
+++ resources/views/auth/portal.blade.php
@@ -1,33 +1,35 @@
 @extends('layouts.app')
+
+
 @section('content')
 <div class="container">
     <div class="row">
         <div class="col-md-8 col-md-offset-2">
             <div class="panel panel-default">
                 <div class="panel-heading">Projects</div>
-                <div class="panel-body">
-		<div class="col-md-6">
-                <div class="hovereffect portfolioImage">
-                    <img class="img-responsive center-block" width="auto" height="auto" src="/images/lone.png">
-                    <div class="overlay">
-                        <h2>Lone Wolf</h2>
-                        <a class="info" href="/lwhome">link here</a>
-                    </div>
-                </div>
-            </div>
-            <div class="col-md-6">
-                <div class="hovereffect portfolioImage">
-                    <img class="img-responsive center-block" width="auto" height="auto" src="/images/guitar.png">
-                    <div class="overlay">
-                        <h2>Guitar Lesson</h2>
-                        <a class="info" href="guitar">link here</a>
-                    </div>
-                </div>
-            </div>
-		</div>
-		</div>
+                    <div class="panel-body">
+            	    	<div class="col-md-6">
+                            <div class="hovereffect portfolioImage">
+                                <img class="img-responsive center-block" width="auto" height="auto" src="/images/lone.png">
+                                <div class="overlay">
+                                    <h2>Lone Wolf</h2>
+                                    <a class="info" href="/lwhome">link here</a>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-md-6">
+                            <div class="hovereffect portfolioImage">
+                                <img class="img-responsive center-block" width="auto" height="auto" src="/images/guitar.png">
+                                <div class="overlay">
+                                    <h2>Guitar Lesson</h2>
+                                    <a class="info" href="guitar">link here</a>
+                                </div>
+                            </div>
+                        </div>
+		            </div>
+		        </div>
+	        </div>
 	    </div>
-	</div>
     </div>
 </div>
 @endsection
