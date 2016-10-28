6985390d5d124c9752d5cc9cc6ad864644207c0c
Kevin <whiteowl0001@gmail.com>
2016-10-28 05:05:34 +0000

    Added Add link

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -49,9 +49,7 @@
                     <ul class="nav navbar-nav">
                         &nbsp;
                     </ul>
-                    <ul class="nav navbar-nav">
-                        <li><a href="#">Add</a></li>
-                    </ul>
+                 
 
                     <!-- Right Side Of Navbar -->
                     <ul class="nav navbar-nav navbar-right">
@@ -60,6 +58,7 @@
                             <li><a href="{{ url('/login') }}">Login</a></li>
                             <li><a href="{{ url('/register') }}">Register</a></li>
                         @else
+                            <li><a href="#">Add</a></li>
                             <li class="dropdown">
                                 <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">
                                     {{ Auth::user()->name }} <span class="caret"></span>
