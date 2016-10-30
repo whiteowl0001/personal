f88bf68a0acd7cf6611776f90cb494c4d407be37
Kevin <whiteowl0001@gmail.com>
2016-10-30 10:42:01 +0000

    Project Display

--- resources/views/auth/add.blade.php
+++ resources/views/auth/add.blade.php
@@ -14,8 +14,7 @@
                                 <p>Project Title</p>
                             </div>
             	    	    <div class="form-group col-lg-10">
-                                <input type="text" class="form-control" id="ProjectTitle" name="ProjectTitle" placeholder="Title"
-                                 value="{{$projectdisplay->ProjectTitle}}">
+                                <input type="text" class="form-control" id="ProjectTitle" name="ProjectTitle" placeholder="Title">
                                 <br>
                             </div>
                             <div class="col-lg-5">
