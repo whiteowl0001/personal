aaaf3a58508339c280df0da24089986229255a59
Kevin <whiteowl0001@gmail.com>
2016-10-30 04:34:56 +0000

    Add page design adjusted

--- resources/views/auth/add.blade.php
+++ resources/views/auth/add.blade.php
@@ -13,14 +13,14 @@
                             <div class="col-lg-5">
                                 <p>Project Title</p>
                             </div>
-            	    	    <div class="col-lg-10">
+            	    	    <div class="form-group col-lg-10">
                                 <input type="text" class="form-control" id="ProjectTitle" placeholder="Title">
                                 <br>
                             </div>
                             <div class="col-lg-5">
                                 <p>Select Image</p>
                             </div>
-                            <div class="col-lg-10">
+                            <div class="form-group col-lg-10">
                                 <input type="file" name="ProjectImage">
                             </div>
                             <input type="submit" class="pull-right btn btn-sm btn-primary">
