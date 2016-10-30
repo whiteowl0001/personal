dcb3514884d3b60c858779551225dfc596fca89e
Kevin <whiteowl0001@gmail.com>
2016-10-30 04:31:50 +0000

    Add page design adjusted

--- resources/views/auth/add.blade.php
+++ resources/views/auth/add.blade.php
@@ -9,7 +9,7 @@
                 <div class="panel-heading">Add Projects</div>
                     <div class="panel-body">
                         <form enctype="multipart/form-data" action="{{url('/add/project/image')}}" method="POST">
-                        {!! csrf_field() !!}
+                         {{ csrf_field() }}
                             <div class="col-lg-5">
                                 <p>Project Title</p>
                             </div>
@@ -21,9 +21,9 @@
                                 <p>Select Image</p>
                             </div>
                             <div class="col-lg-10">
-                                <input type="file" name="ProfilePicture">
-                                <input type="submit" class="pull-right btn btn-sm btn-primary">
+                                <input type="file" name="ProjectImage">
                             </div>
+                            <input type="submit" class="pull-right btn btn-sm btn-primary">
                         </form>
 		            </div>
 		        </div>
