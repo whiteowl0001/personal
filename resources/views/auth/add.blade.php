080ae730bea2c12ebb5d86343b9cf18c0b2722c4
Kevin <whiteowl0001@gmail.com>
2016-10-30 09:40:22 +0000

    Project Display

--- resources/views/auth/add.blade.php
+++ resources/views/auth/add.blade.php
@@ -9,7 +9,7 @@
                 <div class="panel-heading">Add Projects</div>
                     <div class="panel-body">
                         <form enctype="multipart/form-data" action="{{url('/add/project/image')}}" method="POST">
-                         {{ csrf_field() }}
+                        {!! csrf_field() !!}
                             <div class="col-lg-5">
                                 <p>Project Title</p>
                             </div>
@@ -24,6 +24,7 @@
                                 <input type="file" name="ProjectImage">
                             </div>
                             <input type="submit" class="pull-right btn btn-sm btn-primary">
+                            {!! csrf_field() !!}
                         </form>
 		            </div>
 		        </div>
