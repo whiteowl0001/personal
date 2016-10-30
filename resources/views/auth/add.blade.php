98ab65527e9ca391769eee3d20813560173c1592
Kevin <whiteowl0001@gmail.com>
2016-10-30 09:47:52 +0000

    Project Display

--- resources/views/auth/add.blade.php
+++ resources/views/auth/add.blade.php
@@ -14,14 +14,14 @@
                                 <p>Project Title</p>
                             </div>
             	    	    <div class="form-group col-lg-10">
-                                <input type="text" class="form-control" id="ProjectTitle" placeholder="Title">
+                                <input type="text" class="form-control" id="ProjectTitle" placeholder="Title" value="{{ csrf_token() }}">
                                 <br>
                             </div>
                             <div class="col-lg-5">
                                 <p>Select Image</p>
                             </div>
                             <div class="form-groupcol-lg-10">
-                                <input type="file" name="ProjectImage">
+                                <input type="file" name="ProjectImage" value="{{ csrf_token() }}">
                             </div>
                             <input type="submit" class="pull-right btn btn-sm btn-primary">
                             {!! csrf_field() !!}
