bf9f191407728c80edea61eb21efb64893123a69
Kevin <whiteowl0001@gmail.com>
2016-10-30 21:01:48 +0000

    Project Display

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -58,6 +58,7 @@
                             <li><a href="{{ url('/login') }}">Login</a></li>
                             <li><a href="{{ url('/register') }}">Register</a></li>
                         @else
+                            <li><a href="/portal">Portal</a></li>
                             <li><a href="/add">Add</a></li>
                             <li class="dropdown">
                                 <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">
