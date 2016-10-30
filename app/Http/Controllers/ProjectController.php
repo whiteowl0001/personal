00488f5e68885011bf1a9c63e3aa13afaee846fa
Kevin <whiteowl0001@gmail.com>
2016-10-30 20:45:22 +0000

    Project Display

--- app/Http/Controllers/ProjectController.php
+++ app/Http/Controllers/ProjectController.php
@@ -26,7 +26,7 @@ class ProjectController extends Controller
         if($request->hasFile('ProjectImage')){
             $ProjectImage = $request->file('ProjectImage');
             $filename = time() . '.' . $ProjectImage->getClientOriginalExtension();
-            Image::make($ProjectImage)->resize(300,300)->save( public_path('/images/ProdjectImages/' . $filename));
+            Image::make($ProjectImage)->save( public_path('/images/ProdjectImages/' . $filename));
             
             $ProjectDisplay->ProjectImage = $filename;
         }
