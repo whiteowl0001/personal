a9a86f44fa2b6977a50d3423011e0e523139b9aa
Kevin <whiteowl0001@gmail.com>
2016-10-31 02:35:26 +0000

    Project Display

---
+++ resources/views/auth/LoneWolf/lwpreowned.blade.php
@@ -0,0 +1,47 @@
+@extends('layout.LW_Master')
+
+@section('content')
+<div class="container-fluid containercustom2">
+            <img src="/images/preowned/preowned_banner.jpg"  width="" height="auto">
+    <div class="panel panelcustom">
+        <div class="row">
+            <div class="col-md-4">
+                <img class="imgpre" src="/images/preowned/HD-2.jpg" width="200px" height="auto">
+            </div>
+            <div class="col-md-6">
+                <div class="orange2 ">2015 Harley-Davidson® FLSTNSE - CVO™ Softail® Deluxe</div>
+            </div>
+             <div class="col-md-2">
+                  <div class="orange2">$27,995.00</div>
+            </div>
+             <div class="col-sm-8">
+                  <div class="underline"></div>
+            </div>
+            <div class="col-md-1">
+                <div class="whitecolor">Stock:</div>
+                <div class="whitecolor">Condition:</div>
+            </div>
+            <div class="col-md-2">
+                <div class="whitecolor">UH95986</div>
+                 <div class="whitecolor">Pre-Owned </div>
+            </div>
+            <div class="col-md-1">
+                <div class="whitecolor">Color:</div>
+                <div class="whitecolor"> Milage:</div>
+            </div>
+            <div class="col-md-2">
+                <div class="whitecolor">BLACK TUNGSTEN</div>
+                <div class="whitecolor">5,000</div>
+            </div>
+                <div class="col-md-2">
+                  <img src="/images/harly.png" width="70px" height="auto">
+                </div>
+            <div class="btn-group preowned-text">
+                    <a href="#" class="btn btn-finance"><i class="fa fa-comment-o fa-2x icon-orange" aria-hidden="true"></i>Get a quote</a>
+                    <a href="#" class="btn btn-finance"><i class="fa fa-arrows-h fa-2x icon-orange" aria-hidden="true"></i>Value Your Trade</a>
+                    <a href="#" class="btn btn-finance"><i class="fa fa-money fa-2x icon-orange" aria-hidden="true"></i>Get finance</a>
+                    <a href="#" class="btn btn-finance2">View Details</a>
+            </div>
+        </div>
+    </div>
+@stop
