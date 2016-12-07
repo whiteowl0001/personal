aaa0e54397b54cf0d82b484a11206ebf12f6a4e4
Kevin <whiteowl0001@gmail.com>
2016-12-07 11:27:01 +0000

    added dynamically generated blog

--- resources/views/auth/add.blade.php
+++ resources/views/auth/add.blade.php
@@ -11,6 +11,7 @@
                         <form enctype="multipart/form-data" action="{{url('/add/project/image')}}" method="POST">
                         {!! csrf_field() !!}
                             <div class="col-lg-5">
+                                                                <!--Add Project-->
                                 <p>Project Title</p>
                             </div>
             	    	    <div class="form-group col-lg-10">
@@ -24,7 +25,29 @@
                                 <input type="file" id="ProjectImage" name="ProjectImage">
                             </div>
                             <input type="submit" class="pull-right btn btn-sm btn-primary">
-                        </form>
+                            <div class="col-lg-5">
+		            </div>
+		        </div>
+		        <div class="panel-heading">Add Blog</div>
+                    <div class="panel-body">
+                        <form enctype="multipart/form-data" action="{{url('/add/project/image')}}" method="POST">
+                        {!! csrf_field() !!}
+                            <div class="col-lg-5">
+                                                                <!--Add Blog-->
+                                <p>Blog Title</p>
+                            </div>
+            	    	    <div class="form-group col-lg-10">
+                                <input type="text" class="form-control" id="ProjectTitle" name="ProjectTitle" placeholder="Title">
+                                <br>
+                            </div>
+                            <div class="col-lg-5">
+                                <p>Select Image</p>
+                            </div>
+                            <div class="form-group col-lg-10">
+                                <input type="file" id="ProjectImage" name="ProjectImage">
+                            </div>
+                            <input type="submit" class="pull-right btn btn-sm btn-primary">
+                            <div class="col-lg-5">
 		            </div>
 		        </div>
 	        </div>
