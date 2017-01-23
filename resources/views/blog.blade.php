c0bfdd2dc230d5e48c08cc12e117e25095b8eb3e
Kevin <whiteowl0001@gmail.com>
2017-01-23 06:46:45 +0000

    blog

--- resources/views/blog.blade.php
+++ resources/views/blog.blade.php
@@ -1,5 +1,26 @@
 @extends('layout.master')
 @section('content')
-
+<div class="blog-list">
+    <article>
+        <header>
+            <h1>Web Crawling: Php Dom Parsing</h1>
+            <div>
+                <span>
+                    Kevin Morales:
+                </span>
+            </div>
+        </header>
+        <div class="blog-content">
+            <div class="blog-thumb">
+                <a href="#">
+                    <img src="/images/ProdjectImages/crawler.jpg"></img>
+                </a>
+            </div>
+            <div class="blog-summary">
+                
+            </div>
+        </div>
+    </article>
+</div>
 
 @stop
