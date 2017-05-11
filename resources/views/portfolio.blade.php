24e66cf898326aa76442757f09fa1f9fc8459821
Kevin <whiteowl0001@gmail.com>
2017-05-11 04:50:51 +0000

    css update

--- resources/views/portfolio.blade.php
+++ resources/views/portfolio.blade.php
@@ -1,9 +1,111 @@
 @extends('layout.master')
 @section('content')
 
+<div id="portfolio">
+			<div class="container">
+				<div class="row">
+
+					<div class="col-xs-12 col-sm-8 col-md-6 col-sm-offset-2 col-md-offset-3">
+						<div class="portfolio-heading">
+							<h2>portfolio</h2> 
+							<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent metus magna,malesuada porta elementum vitae.</p>
+						</div>
+					</div>
+
+				</div>   	
+	    	</div>
+
+	    	<div class="portfolio-thumbnail">
+	    		<div class="container-fluid">
+	    			<div class="row">
+
+	    				<div class="col-xs-6 col-sm-3 col-md-3">
+	    					<div class="item">
+	    						<img src="images/MyBiz/interior/interior1.jpg" alt="">
+	    						<div class="caption">
+                               		<i class="fa fa-search" aria-hidden="true"></i>
+                               		<p>Interior Painting</p>
+                            	</div>
+	    					</div>
+	    				</div>
+
+	    				<div class="col-xs-6 col-sm-3 col-md-3">
+	    					<div class="item">
+	    						<img src="images/MyBiz/interior/interior2.jpg" alt="">
+	    						<div class="caption">
+                               		<i class="fa fa-search" aria-hidden="true"></i>
+                               		<p>Interior Painting</p>
+                            	</div>
+	    					</div>
+	    				</div>
+
+	    				<div class="col-xs-6 col-sm-3 col-md-3">
+	    					<div class="item">
+	    						<img src="images/MyBiz/interior/interior3.jpg" alt="">
+	    						<div class="caption">
+                               		<i class="fa fa-search" aria-hidden="true"></i>
+                               		<p>Interior Painting</p>
+                            	</div>
+	    					</div>
+	    				</div>
+	    				
+        	            <div class="col-xs-6 col-sm-3 col-md-3">
+	    					<div class="item">
+	    						<img src="images/MyBiz/interior/interior4.jpg" alt="">
+	    						<div class="caption">
+                               		<i class="fa fa-search" aria-hidden="true"></i>
+                               		<p>Interior Painting</p>
+                            	</div>
+	    					</div>
+	    				</div>
+	    				
+	    				<div class="col-xs-6 col-sm-3 col-md-3">
+	    					<div class="item">
+	    						<img src="images/MyBiz/exterior/exterior1.jpg" alt="">
+	    						<div class="caption">
+                               		<i class="fa fa-search" aria-hidden="true"></i>
+                               		<p>Exterior Painting</p>
+                            	</div>
+	    					</div>
+	    				</div>
+
+	    				<div class="col-xs-6 col-sm-3 col-md-3">
+	    					<div class="item">
+	    						<img src="images/MyBiz/exterior/exterior2.jpg" alt="">
+	    						<div class="caption">
+                               		<i class="fa fa-search" aria-hidden="true"></i>
+                               		<p>Exterior Painting</p>
+                            	</div>
+	    					</div>
+	    				</div>
+
+	    				<div class="col-xs-6 col-sm-3 col-md-3">
+	    					<div class="item">
+	    						<img src="images/MyBiz/exterior/exterior3.jpg" alt="">
+	    						<div class="caption">
+                               		<i class="fa fa-search" aria-hidden="true"></i>
+                               		<p>Exterior Painting</p>
+                            	</div>
+	    					</div>
+	    				</div>
+
+	    				<div class="col-xs-6 col-sm-3 col-md-3">
+	    					<div class="item">
+	    						<img src="images/MyBiz/exterior/exterior4.jpg" alt="">
+	    						<div class="caption">
+                               		<i class="fa fa-search" aria-hidden="true"></i>
+                               		<p>Exterior Painting</p>
+                            	</div>
+	    					</div>
+	    				</div>
+
+	    			</div>
+	    		</div>
+	    	</div>
+		</div>
 
 
-
+<!--
 <div class="container-fluid container-portfolio">
         <div class="row paddingPort">
             <div class="col-md-4">
@@ -25,6 +127,6 @@
                 </div>
             </div>
         </div>
-</div>
+</div> -->
 
 @stop
