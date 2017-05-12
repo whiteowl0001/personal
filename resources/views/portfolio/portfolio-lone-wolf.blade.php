842a808f2d0d2db82700e9cc4544da85a9309614
Kevin <whiteowl0001@gmail.com>
2017-05-12 08:42:45 +0000

    portfolio change

---
+++ resources/views/portfolio/portfolio-lone-wolf.blade.php
@@ -0,0 +1,180 @@
+@extends('layout.LW_Master')
+
+@section('content')
+
+<div class="container-fluid containercustom2">
+            <img src="/images/preowned/preowned_banner.jpg"  width="" height="auto">
+    <div class="panel panelcustom">
+        <div class="row">
+            <div class="col-md-4">
+                <img class="imgpre" src="/images/preowned/HD-2.jpg" width="200px" height="auto">
+            </div>
+            <div class="col-md-6">
+                <div class="orange2 ">2015 Harley-Davidson® FLSTNSE - CVO™ Softail® Deluxe</div>
+            </div>
+             <div class="col-md-2">
+                  <div class="orange2">$27,995.00</div>
+            </div>
+             <div class="col-sm-8">
+                  <div class="underline"></div>
+            </div>
+            <div class="col-md-1 col-xs-6">
+                <div class="whitecolor">Stock:</div>
+                <div class="whitecolor">Condition:</div>
+            </div>
+            <div class="col-md-2 col-xs-6">
+                <div class="whitecolor">UH95986</div>
+                 <div class="whitecolor">Pre-Owned </div>
+            </div>
+            <div class="col-md-1 col-xs-6">
+                <div class="whitecolor">Color:</div>
+                <div class="whitecolor"> Milage:</div>
+            </div>
+            <div class="col-md-2 col-xs-6">
+                <div class="whitecolor">BLACK TUNGSTEN</div>
+                <div class="whitecolor">5,000</div>
+            </div>
+                <div class="col-md-2">
+                  <img src="/images/harly.png" width="70px" height="auto">
+                </div>
+            <div class="col-md-8 col-xs-12 btn-group preowned-text">
+                    <a href="#" class="btn btn-finance"><i class="fa fa-comment-o fa-2x icon-orange" aria-hidden="true"></i>Get a quote</a>
+                    <a href="#" class="btn btn-finance"><i class="fa fa-arrows-h fa-2x icon-orange" aria-hidden="true"></i>Value Your Trade</a>
+                    <a href="#" class="btn btn-finance"><i class="fa fa-money fa-2x icon-orange" aria-hidden="true"></i>Get finance</a>
+                    <a href="#" class="btn btn-finance2">View Details</a>
+            </div>
+        </div>
+    </div>
+    
+    <div class="panel panelcustom2">
+         <div class="row">
+            <div class="col-md-4">
+                 <img class="imgpre" src="/images/preowned/HD-4.jpg" width="200px" height="auto">
+            </div>
+            <div class="col-md-6">
+                <div class="orange2 ">2015 Harley-Davidson® FXSB - Softail® Breakout®</div>
+            </div>
+             <div class="col-md-2">
+                  <div class="orange2"> $17,295.00</div>
+            </div>
+             <div class="col-sm-8">
+                  <div class="underline"></div>
+            </div>
+            <div class="col-md-1 col-xs-6">
+                <div class="whitecolor">Stock:</div>
+                <div class="whitecolor">Condition:</div>
+            </div>
+            <div class="col-md-2 col-xs-6">
+                <div class="whitecolor">UH-019299</div>
+                 <div class="whitecolor">Pre-Owned</div>
+            </div>
+            <div class="col-md-1 col-xs-6">
+                <div class="whitecolor">Color:</div>
+                <div class="whitecolor"> Milage:</div>
+            </div>
+            <div class="col-md-2 col-xs-6">
+                <div class="whitecolor">RED SUNGLO</div>
+                <div class="whitecolor">4,970</div>
+            </div>
+                <div class="col-md-2 ">
+                  <img src="/images/harly.png" width="70px" height="auto">
+                </div>
+            <div class="col-md-8 col-xs-12 btn-group preowned-text">
+                    <a href="#" class="btn btn-finance"><i class="fa fa-comment-o fa-2x icon-orange" aria-hidden="true"></i>Get a quote</a>
+                    <a href="#" class="btn btn-finance"><i class="fa fa-arrows-h fa-2x icon-orange" aria-hidden="true"></i>Value Your Trade</a>
+                    <a href="#" class="btn btn-finance"><i class="fa fa-money fa-2x icon-orange" aria-hidden="true"></i>Get finance</a>
+                    <a href="#" class="btn btn-finance2">View Details</a>
+            </div>
+        </div>
+    </div>
+    <div class="panel panelcustom">
+        <div class="row">
+            <div class="col-md-4">
+                 <img class="imgpre" src="/images/preowned/HD-3.jpg" width="200px" height="auto">
+            </div>
+            <div class="col-md-6">
+                <div class="orange2 ">2016 Harley-Davidson® FLD - Dyna® Switchback™</div>
+            </div>
+             <div class="col-md-2">
+                  <div class="orange2"> $16,995.00</div>
+            </div>
+             <div class="col-sm-8">
+                  <div class="underline"></div>
+            </div>
+            <div class="col-md-1 col-xs-6">
+                <div class="whitecolor">Stock:</div>
+                <div class="whitecolor">Condition:</div>
+            </div>
+            <div class="col-md-2 col-xs-6">
+                <div class="whitecolor">UH-307201 </div>
+                 <div class="whitecolor">Pre-Owned </div>
+            </div>
+            <div class="col-md-1 col-xs-6">
+                <div class="whitecolor">Color:</div>
+                <div class="whitecolor"> Milage:</div>
+            </div>
+            <div class="col-md-2 col-xs-6">
+                <div class="whitecolor">VIVID BLACK</div>
+                <div class="whitecolor">1,651</div>
+            </div>
+                <div class="col-md-2">
+                  <img src="/images/harly.png" width="70px" height="auto">
+                </div>
+            <div class="col-md-8 col-xs-12 btn-group preowned-text">
+                    <a href="#" class="btn btn-finance"><i class="fa fa-comment-o fa-2x icon-orange displaypadding" aria-hidden="true"></i>Get a quote</a>
+                    <a href="#" class="btn btn-finance"><i class="fa fa-arrows-h fa-2x icon-orange displaypadding" aria-hidden="true"></i>Value Your Trade</a>
+                    <a href="#" class="btn btn-finance"><i class="fa fa-money fa-2x icon-orange displaypadding" aria-hidden="true"></i>Get finance</a>
+                    <a href="#" class="btn btn-finance2">View Details</a>
+            </div>
+        </div>
+    </div>
+</div>
+
+
+
+                                        <!-------------footer----------->
+                                                
+ <div class="container-fluid footermain">
+            <ul class=" col-md-2  dashright">
+				<li class="uppercase  orange">QUICK LINKS</li>
+					<div class="white">
+					<li><a href="#">Service Department</a></li>
+					<li><a href="#">Parts Department</a></li>
+					<li><a href="#">Events Calendar</a></li>
+					<li><a href="#">Financing</a></li>		
+					<li><a href="#">Test Ride</a></li>
+					</div>
+			</ul>
+			<ul class="col-md-3  dashright">
+					<li class="uppercase orange">LONE WOLF H-D</li>
+					<div class="white">
+					<li><a href="#">19011 E Cataldo Ave</a></li>
+					<li><a href="#">Spokane Valley, WA 99016</a></li>
+					<li class="orange">Phone:<a>(509)-927-7433</a></li>		
+					<li class="orange">Fax:<a>(509)-927-3306</a></li>
+					<li class="uppercase orange">Map & Hours</li>
+					</div>
+			</ul>
+			<ul class="col-md-2  dashright">
+					<li class="uppercase orange">FIND A BIKE</li>
+					<div class="white">
+					<li><a href="#">New Inventory</a></li>
+					<li><a href="#">Pre-Owned</a></li>
+					<li><a href="#">Financing</a></li>		
+					<li><a href="#">Get A Quote</a></li>
+					<a></a>
+					</div>
+			</ul>
+				<ul class="col-md-4 ">
+		        	<a class="btn btn-social-icon btn-facebook">
+                    <span class="fa fa-facebook"></span>
+                    </a>
+                    <a class="btn btn-social-icon btn-google">
+                    <span class="fa fa-google"></span>
+                    </a>
+                    <a class="btn btn-social-icon btn-instagram">
+                    <span class="fa fa-instagram"></span>
+                    </a>
+			</ul>
+    </div>
+@stop
